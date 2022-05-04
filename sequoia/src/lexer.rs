11c4ce13a8e8b25d22d4ce14e38890d5e55d3fda
MrKomodoDragon <svrchn921@outlook.com>
2022-05-03 19:27:34 -0700

    Initial work on lexer (more stuff to be added soon:tm:

--- sequoia/src/lexer.rs
+++ sequoia/src/lexer.rs
@@ -2,5 +2,52 @@ use logos::Logos;
 
 #[derive(Logos, Debug, PartialEq)]
 enum Token {
-	
+	// Conditionals
+	#[token("&&")]
+	And,
+
+	#[token("||")]
+	Or,
+
+	//Equality
+	#[token("==")]
+	Equal,
+
+	#[token("!=")]
+	NotEqual,
+
+	#[token(">=")]
+	GreaterOrEqual,
+
+	#[token(">")]
+	GreaterThan,
+
+	#[token("<=")]
+	LessOrEqual,
+
+	//EOL chars
+	#[token(";")]
+	Semicolon,
+
+	#[token(":")]
+	Colon,
+
+
+
+	//Arithmetic operators
+	#[token("+")]
+	Plus,
+
+	#[token("*")]
+	Multiply,
+
+	#[token("/")]
+	Divide,
+
+	#[token("-")]
+	Subtract,
+
+	#[token("%")]
+	Modulus,
+
 }
